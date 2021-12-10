d60c4f4e837b5a9c75a29f764e5fed7e321ad317
s1paris <me@s1paris.ru>
2021-12-10 10:12:29 +0300

    fix(code): solution was changed in accordance with the description of the task

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const makePlayer = ({name, hp, img, weapon} = {})  => {
+const makePlayer = ({name, hp, img, weapon = []} = {})  => {
     return {
         name,
         hp,
@@ -11,26 +11,55 @@ const makePlayer = ({name, hp, img, weapon} = {})  => {
 }
 
 const createPlayer = (playerPosition, player) => {
-    const playerElement = document.createElement('div')
-    playerElement.classList.add(playerPosition)
-    playerElement.innerHTML = `
-        <div class="progressbar">
-            <div class="life" style="width: 100%"></div>
-            <div class="name">${player.name}</div>
-        </div>
-        <div class="character">
-            <img src="${player.img}"  alt="${player.name}"/>
-        </div>
-    `
+    const makeElement = ({tagName = '', className = '', styles = {}, attributes = {}, text = ''} = {}) => {
+        const element = document.createElement(tagName)
 
-    document.getElementsByClassName('arenas')[0].appendChild(playerElement)
+        if (className !== '') {
+            element.classList.add(className)
+        }
+
+        if (text.length > 0) {
+            element.innerText = text
+        }
+
+        if (Object.keys(styles).length > 0) {
+            let cssText = ''
+
+            for (const styleKey in styles) {
+                cssText += `${styleKey}:${styles[styleKey]};`
+            }
+
+            element.style.cssText = cssText
+        }
+
+        if (Object.keys(attributes).length > 0) {
+            for (const attributeKey in attributes) {
+                element.setAttribute(attributeKey, attributes[attributeKey])
+            }
+        }
+
+        return element
+    }
+
+    const character = makeElement({tagName: 'div', className: playerPosition})
+    const characterAvatar = makeElement({tagName: 'img', attributes: {alt: player.name, src: player.img}})
+    const characterLifeBar = makeElement({tagName: 'div', className: 'life', styles: {width: `${player.hp}%`}})
+    const characterName = makeElement({tagName: 'div', className: 'name', text: player.name})
+    const characterAvatarContainer = makeElement({tagName: 'div', className: 'character'})
+    const characterProgressbar = makeElement({tagName: 'div', className: 'progressbar'})
+
+    characterProgressbar.append(characterLifeBar, characterName)
+    characterAvatarContainer.append(characterAvatar)
+
+    character.append(characterProgressbar, characterAvatarContainer)
+
+    document.getElementsByClassName('arenas')[0].appendChild(character)
 }
 
-const players = [
-    makePlayer({name: 'Scorpion', hp: 100, img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif', weapon: 'knife'}),
-    makePlayer({name: 'Sonya', hp: 100, img: 'http://reactmarathon-api.herokuapp.com/assets/sonya.gif', weapon: 'boobs'})
-]
 
-players.forEach((value, index) => {
-    createPlayer(`player${index + 1}`, value)
-})
+const playerOne = makePlayer({name: 'Scorpion', hp: 30, img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif', weapon: ['knife', 'fireball']})
+const playerTwo = makePlayer({name: 'Sonya', hp: 100, img: 'http://reactmarathon-api.herokuapp.com/assets/sonya.gif', weapon: ['boobs', 'gun']})
+
+
+createPlayer(`player1`, playerOne)
+createPlayer(`player2`, playerTwo)
